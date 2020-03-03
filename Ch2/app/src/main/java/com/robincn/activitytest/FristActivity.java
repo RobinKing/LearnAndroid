b2796a4fb9b635ae1165b896a4e0973b96cbb3cd
Robin King <jialei.jing@gmail.com>
2020-03-04 00:08:07 +0800

    <new> After Ch2.2 Activity. New Activity by hand.

---
+++ Ch2/app/src/main/java/com/robincn/activitytest/FristActivity.java
@@ -0,0 +1,14 @@
+package com.robincn.activitytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FristActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+    }
+}
