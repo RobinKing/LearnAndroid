4ad757a99d9252ea2c1e775735478cfbc996a8ee
Robin King <jialei.jing@gmail.com>
2020-03-04 22:52:54 +0800

    <fix> Ch2.2 After Toast and menu

--- Ch2/app/src/main/java/com/robincn/activitytest/FristActivity.java
+++ Ch2/app/src/main/java/com/robincn/activitytest/FristActivity.java
@@ -3,6 +3,11 @@ package com.robincn.activitytest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FristActivity extends AppCompatActivity {
 
@@ -10,5 +15,33 @@ public class FristActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FristActivity.this,"You clicked Button", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(FristActivity.this,"You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(FristActivity.this,"You clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
