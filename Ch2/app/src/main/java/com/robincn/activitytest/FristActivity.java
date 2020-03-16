6eb9161c4a5499a515a053cd4aad80f8b7acf40f
Robin King <jialei.jing@gmail.com>
2020-03-16 22:38:07 +0800

    <new> After Ch2. Known more about activity. some tricks of get Activity name and finish the program from any activities.

--- Ch2/app/src/main/java/com/robincn/activitytest/FristActivity.java
+++ Ch2/app/src/main/java/com/robincn/activitytest/FristActivity.java
@@ -13,7 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class FristActivity extends AppCompatActivity {
+public class FristActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
