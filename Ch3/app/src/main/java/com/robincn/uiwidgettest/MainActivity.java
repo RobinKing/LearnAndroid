64a20d67ce0eb6c9e294c4010c0ada9669505655
robin <jingjl1001@gmail.com>
2020-03-18 23:16:20 +0800

    <fix> After Ch3.2.2. Learned something about TextView and Buton.

--- Ch3/app/src/main/java/com/robincn/uiwidgettest/MainActivity.java
+++ Ch3/app/src/main/java/com/robincn/uiwidgettest/MainActivity.java
@@ -7,25 +7,18 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(this);
-    }
-
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.button:
-                Log.d("TAG", "button clicked");
-                // do something
-                break;
-            default:
-                Log.d("TAG", "nothing");
-                break;
-        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Test", "button clicked");
+            }
+        });
     }
 }
