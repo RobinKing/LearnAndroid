5cf55706c5cb344850862fd9d91b237fa7e10581
robin <jingjl1001@gmail.com>
2020-03-18 23:12:06 +0800

    <new> After Ch2.3.2. Another way to handle click event on button.

--- Ch3/app/src/main/java/com/robincn/uiwidgettest/MainActivity.java
+++ Ch3/app/src/main/java/com/robincn/uiwidgettest/MainActivity.java
@@ -3,21 +3,29 @@ package com.robincn.uiwidgettest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // something todo when button was clicked
-            }
-        });
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                Log.d("TAG", "button clicked");
+                // do something
+                break;
+            default:
+                Log.d("TAG", "nothing");
+                break;
+        }
     }
 }
