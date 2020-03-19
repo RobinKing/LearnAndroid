7d2746a42196a2519bc4ef07aef154f2d5c21beb
robin <jingjl1001@gmail.com>
2020-03-19 23:22:42 +0800

    <new> After 3.2.3. Learned something about EditText.

--- Ch3/app/src/main/java/com/robincn/uiwidgettest/MainActivity.java
+++ Ch3/app/src/main/java/com/robincn/uiwidgettest/MainActivity.java
@@ -6,19 +6,37 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editText;
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                String inputText = editText.getText().toString();
+                Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        editText = (EditText) findViewById(R.id.edit_text);
+        button.setOnClickListener(this);
+/*        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("Test", "button clicked");
             }
-        });
+        });*/
     }
 }
