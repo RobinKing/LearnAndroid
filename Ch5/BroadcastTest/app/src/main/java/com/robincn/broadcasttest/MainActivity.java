fbe21b2927286c4678980ecd690b9bf67f00c85d
robin <jingjl1001@gmail.com>
2020-05-12 23:04:34 +0800

    <new> After Ch5.4, after local broadcast.

--- Ch5/BroadcastTest/app/src/main/java/com/robincn/broadcasttest/MainActivity.java
+++ Ch5/BroadcastTest/app/src/main/java/com/robincn/broadcasttest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.robincn.broadcasttest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
@@ -21,26 +22,37 @@ public class MainActivity extends AppCompatActivity {
 
     private NetworkChangeReceiver networkChangeReceiver;
 
+    private LocalReceiver localReceiver;
+    private LocalBroadcastManager localBroadcastManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                Log.d("RobinCN", "onClick: button");
-                Intent intent = new Intent("com.robincn.broadcaster.MY_BROADCAST");
-                intent.setComponent(new ComponentName("com.robincn.broadcasttest",
-                        "com.robincn.broadcasttest.MyBroadcastReceiver"));
-                sendBroadcast(intent);
-                intent.setComponent(new ComponentName("com.robincn.broadcasttest2",
-                        "com.robincn.broadcasttest2.AnotherBroadcastReceiver"));
-                sendBroadcast(intent);
+                Intent intent = new Intent("com.robincn.broadcasttest.LOCAL_BROADCAST");
+                localBroadcastManager.sendBroadcast(intent);
             }
+            //            @Override
+//            public void onClick(View v) {
+//                Log.d("RobinCN", "onClick: button");
+//                Intent intent = new Intent("com.robincn.broadcaster.MY_BROADCAST");
+//                intent.setComponent(new ComponentName("com.robincn.broadcasttest",
+//                        "com.robincn.broadcasttest.MyBroadcastReceiver"));
+//                sendBroadcast(intent);
+//                intent.setComponent(new ComponentName("com.robincn.broadcasttest2",
+//                        "com.robincn.broadcasttest2.AnotherBroadcastReceiver"));
+//                sendBroadcast(intent);
+//            }
         });
         intentFilter = new IntentFilter();
+        intentFilter.addAction("com.robincn.broadcasttest.LOCAL_BROADCAST");
+        localReceiver = new LocalReceiver();
+        localBroadcastManager.registerReceiver(localReceiver, intentFilter);
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         networkChangeReceiver = new NetworkChangeReceiver();
         registerReceiver(networkChangeReceiver, intentFilter);
@@ -50,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         unregisterReceiver(networkChangeReceiver);
+        localBroadcastManager.unregisterReceiver(localReceiver);
     }
 
     class NetworkChangeReceiver extends BroadcastReceiver {
@@ -63,6 +76,13 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show();
             }
 //            Toast.makeText(context, "network change", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    class LocalReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "received local broadcast", Toast.LENGTH_SHORT).show();
         }
     }
 
