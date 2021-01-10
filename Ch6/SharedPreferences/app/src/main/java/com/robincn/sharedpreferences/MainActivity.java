cfd35b88147733c34bfb7761b0ebf8e3e11e2c92
robin <jingjl1001@gmail.com>
2021-01-10 15:37:56 +0800

    <fix> After 6.3.2, using SharedPreferences to read and store data.

--- Ch6/SharedPreferences/app/src/main/java/com/robincn/sharedpreferences/MainActivity.java
+++ Ch6/SharedPreferences/app/src/main/java/com/robincn/sharedpreferences/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,11 +18,24 @@ public class MainActivity extends AppCompatActivity {
         saveData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences.Editor editor = getSharedPreferences("data.xml", MODE_PRIVATE).edit();
+                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
                 editor.putString("name", "Tom");
                 editor.putInt("age", 28);
                 editor.putBoolean("married", false);
                 editor.apply();
+            }
+        });
+        Button restoreData = (Button) findViewById(R.id.restore_data);
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+                String name = pref.getString("name", "");
+                int age = pref.getInt("age", 0);
+                boolean married = pref.getBoolean("married", false);
+                Log.d("MainActivity", "name is " + name);
+                Log.d("MainActivity", "age is " + age);
+                Log.d("MainActivity", "married is " + married);
             }
         });
     }
