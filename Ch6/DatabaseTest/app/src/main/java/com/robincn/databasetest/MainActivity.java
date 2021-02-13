595ea160f0fbb90bff496105b092b1d4999d8526
robin <jingjl1001@gmail.com>
2021-02-13 21:23:59 +0800

    <new> CRUD of SQL operation. After Ch6.4.5

--- Ch6/DatabaseTest/app/src/main/java/com/robincn/databasetest/MainActivity.java
+++ Ch6/DatabaseTest/app/src/main/java/com/robincn/databasetest/MainActivity.java
@@ -2,6 +2,8 @@ package com.robincn.databasetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +21,43 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 dbHelper.getWritableDatabase();
+            }
+        });
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("name", "The Da vinci Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                db.insert("Book", null, values);
+                values.clear();
+                values.put("name", "The Lost symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                db.insert("Book", null, values);
+            }
+        });
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("price", 10.99);
+                db.update("Book", values, "name = ?", new String[] {"The Da vinci Code"});
+            }
+        });
+        Button deleteData = (Button) findViewById(R.id.delete_data);
+        deleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.delete("Book", "id>?", new String[] {"2"});
             }
         });
     }
