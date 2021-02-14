ca12da435930ad7fbdb15f1a8478f32671334c3d
robin <jingjl1001@gmail.com>
2021-02-15 00:03:15 +0800

    <new> Learn to use litepal.

---
+++ Ch6/LitePalTest/app/src/main/java/com/robincn/litepaltest/MainActivity.java
@@ -0,0 +1,26 @@
+package com.robincn.litepaltest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.litepal.LitePal;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button createDatabase = (Button) findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LitePal.getDatabase();
+            }
+        });
+    }
+}
