594c734e816a452e120c720d563d0f656f1cf20f
robin <jingjl1001@gmail.com>
2021-02-15 11:18:17 +0800

    <fix>After Ch6. Learned sql.

--- Ch6/LitePalTest/app/src/main/java/com/robincn/litepaltest/MainActivity.java
+++ Ch6/LitePalTest/app/src/main/java/com/robincn/litepaltest/MainActivity.java
@@ -3,10 +3,14 @@ package com.robincn.litepaltest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import org.litepal.LitePal;
+import org.litepal.crud.LitePalSupport;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,6 +40,59 @@ public class MainActivity extends AppCompatActivity {
                 book.save();
                 book.setPrice(10.99);
                 book.save();
+            }
+        });
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Book book = new Book();
+                book.setPrice(14.95);
+                book.setPress("Anchor");
+                book.updateAll("name = ? and author = ?", "The Lost Symbol", "Dan Brown");
+            }
+        });
+        Button updateDataToDefault = (Button) findViewById(R.id.sql_update_data);
+        updateDataToDefault.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Book book = new Book();
+                book.setPages(0);
+                book.setToDefault("pages");
+                book.updateAll("pages < ?", "500");
+            }
+        });
+        Button deleteButton = (Button) findViewById(R.id.delete_data);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LitePal.deleteAll(Book.class, "price < ?", "15");
+            }
+        });
+        Button queryButton = (Button) findViewById(R.id.query_data);
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List<Book> books = LitePal.findAll(Book.class);
+                for (Book book: books) {
+                    Log.d("MainActivity", "book name is " + book.getName());
+                    Log.d("MainActivity", "book author is " + book.getAuthor());
+                    Log.d("MainActivity", "book pages is " + book.getPages());
+                    Log.d("MainActivity", "book price is " + book.getPrice());
+                    Log.d("MainActivity", "book press is " + book.getPress());
+                }
+            }
+        });
+        Button queryButtonMore = (Button) findViewById(R.id.sql_query_data);
+        queryButtonMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List<Book> books = LitePal.select("name", "pages")
+                        .where("pages < 600").offset(0).limit(3).find(Book.class);
+                for (Book book: books) {
+                    Log.d("MainActivity", "Book name is " + book.getName());
+                    Log.d("MainActivity", "Book author is " + book.getAuthor());
+                }
             }
         });
     }
