5cde78b65abaf6a12a8a79a536fe56332eb998a7
robin <jingjl1001@gmail.com>
2021-05-20 13:26:05 +0800

    <new>Begin CH7.4. Start my own provider

--- Ch6/DatabaseTest/app/src/main/java/com/robincn/databasetest/MyDatabaseHelper.java
+++ Ch6/DatabaseTest/app/src/main/java/com/robincn/databasetest/MyDatabaseHelper.java
@@ -29,7 +29,7 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_BOOK);
         db.execSQL(CREATE_CATEGORY);
-        Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_LONG);
+//        Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_LONG);
     }
 
     @Override
